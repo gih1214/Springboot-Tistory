b8228a0041de628291d975fd218689e8c16fbe3c
gih1214 <gih1214@hanmail.net>
2022-04-06 23:11:09 +0900

    시큐리티 인증 필터 설정

---
+++ src/main/java/site/metacoding/blogv3/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package site.metacoding.blogv3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티가 활성화됨
+@Configuration // IoC 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 인증 설정 메서드
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // super.configure(http);
+        http.csrf().disable(); // csrf 안 쓰면 postman 테스트 못함
+        http.authorizeRequests()
+                .antMatchers("/s/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .defaultSuccessUrl("/");
+    }
+}
