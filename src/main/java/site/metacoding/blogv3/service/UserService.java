849b7a0820ab2eec4e803fefb2e2e2c845fa1916
gih1214 <gih1214@hanmail.net>
2022-04-13 15:30:24 +0900

    비밀번호 초기화 이메일 보내기

--- src/main/java/site/metacoding/blogv3/service/UserService.java
+++ src/main/java/site/metacoding/blogv3/service/UserService.java
@@ -9,6 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.domain.user.User;
 import site.metacoding.blogv3.domain.user.UserRepository;
+import site.metacoding.blogv3.handler.ex.CustomException;
+import site.metacoding.blogv3.util.email.EmailUtil;
+import site.metacoding.blogv3.web.dto.user.PasswordResetReqDto;
 
 @RequiredArgsConstructor
 @Service // IoC 등록
@@ -17,6 +20,27 @@ public class UserService {
     // DI
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final EmailUtil emailUtil;
+
+    @Transactional
+    public void 패스워드초기화(PasswordResetReqDto passwordResetReqDto) {
+        // 1. username, email 이 같은 것이 있는지 체크 (DB에서)
+        Optional<User> userOp = userRepository.findByUsernameAndEmail(passwordResetReqDto.getUsername(),
+                passwordResetReqDto.getEmail());
+
+        // 2. 같은 것이 있다면 DB에 password 초기화 - BCrypt 해시 - update 하기
+        if (userOp.isPresent()) {
+            User userEntity = userOp.get(); // 영속화
+            String encPassword = bCryptPasswordEncoder.encode("9999"); // 실무에선 파일로 넣어서 read 할 것..!!
+            userEntity.setPassword(encPassword);
+        } else {
+            throw new CustomException("해당 이메일이 존재하지 않습니다.");
+        }
+
+        // 3. 초기화된 비밀번호 이메일로 전송
+        emailUtil.sendEmail("보낼 이메일 입력", "비밀번호 초기화", "초기화된 비밀번호 : 9999");
+
+    } // 더티체킹 (update)
 
     @Transactional
     public void 회원가입(User user) { // 서비스는 dto보다 깔끔하게 오브젝트로 받는게 좋다. (웬만하면!!)
--- src/main/java/site/metacoding/blogv3/web/UserController.java
+++ src/main/java/site/metacoding/blogv3/web/UserController.java
@@ -1,22 +1,19 @@
 package site.metacoding.blogv3.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
-import site.metacoding.blogv3.handler.ex.CustomException;
 import site.metacoding.blogv3.service.UserService;
+import site.metacoding.blogv3.util.UtilValid;
 import site.metacoding.blogv3.web.dto.user.JoinReqDto;
+import site.metacoding.blogv3.web.dto.user.PasswordResetReqDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -37,7 +34,24 @@ public class UserController {
         return "/user/joinForm";
     }
 
+    @GetMapping("/user/password-reset-form")
+    public String passwordResetForm() {
+        return "/user/passwordResetForm";
+    }
+
+    // 비밀번호 찾기
+    @PostMapping("/user/password-reset")
+    public String passwordReset(@Valid PasswordResetReqDto passwordResetReqDto, BindingResult bindingResult) {
+
+        UtilValid.요청에러처리(bindingResult);
+
+        userService.패스워드초기화(passwordResetReqDto);
+
+        return "redirect:/login-form";
+    }
+
     // 유저네임 중복체크
+    // ResponseEntity 는 @ResponseBody를 붙이지 않아도 data를 리턴한다.
     @GetMapping("/api/user/username-same-check")
     public ResponseEntity<?> usernameSameCheck(String username) {
         boolean isNotSame = userService.유저네임중복체크(username); // true (같지 않음)
@@ -49,16 +63,7 @@ public class UserController {
     public String join(@Valid JoinReqDto joinReqDto, BindingResult bindingResult) { // 벨리데이션 체크
 
         // System.out.println(bindingResult.hasErrors());
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError fe : bindingResult.getFieldErrors()) {
-                // System.out.println(fe.getField());
-                // System.out.println(fe.getDefaultMessage());
-                errorMap.put(fe.getField(), fe.getDefaultMessage());
-            }
-            // 이부분에서 data리턴인지 html 리턴인지 이것만 구분해서 터트려줘!!
-            throw new CustomException(errorMap.toString());
-        }
+        UtilValid.요청에러처리(bindingResult);
 
         // 핵심 로직
         userService.회원가입(joinReqDto.toEntity());
